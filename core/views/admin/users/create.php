cdea480771a714a2f687b54ec168b92d7a0b6b55
SergeyBor55 <43942010+SergeyBor55@users.noreply.github.com>
2019-01-14 22:51:41 +0300

    Update create.php

--- core/views/admin/users/create.php
+++ core/views/admin/users/create.php
@@ -9,21 +9,21 @@
         <h3>Add user</h3><br>
         <form class="contact" action="#" method="POST">
             <label>Name<br>
-                <input type="text" name="name" placeholder="Jim" value="<?= $name ?>" required="required"></label><br>
+                <input type="text" name="name" placeholder="Jim" value="<?= $options['name'] ?>" required="required"></label><br>
             <label>Surname<br>
-                <input type="text" name="surname" placeholder="Smith" value="<?= $surname ?>"
+                <input type="text" name="surname" placeholder="Smith" value="<?= $options['surname'] ?>"
                        required="required"></label><br>
             <label>Date<br>
-                <input type="date" name="date" value="<?= $date ?>" required="required"></label><br>
+                <input type="date" name="date" value="<?= $options['date'] ?>" required="required"></label><br>
             <label>Login<br>
-                <input type="login" name="login" placeholder="smith2000" value="<?= $login ?>"
+                <input type="login" name="login" placeholder="smith2000" value="<?= $options['login'] ?>"
                        required="required"></label><br>
             <label>Password<br>
-                <input type="password" name="password" value="<?= $password ?>" required="required"></label><br>
+                <input type="password" name="password" value="<?= $options['password'] ?>" required="required"></label><br>
             <label>Gender<br>
                 <select name="gender" required="required">
-                    <option value="1" <?php if ($gender == 1): ?> selected="selected" <?php endif; ?>>Men</option>
-                    <option value="0" <?php if ($gender == 0): ?> selected="selected" <?php endif; ?>>Women</option>
+                    <option value="1" <?php if ($options['gender'] == 1): ?> selected="selected" <?php endif; ?>>Men</option>
+                    <option value="0" <?php if ($options['gender'] == 0): ?> selected="selected" <?php endif; ?>>Women</option>
                 </select>
             </label><br>
             <input type="submit" name="submit" value="Enter"><br>
